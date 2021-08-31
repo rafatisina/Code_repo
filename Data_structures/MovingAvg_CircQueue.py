2d0cf250555eedbdea0de570052ee203bbdabdcc
Sina <rafati@utexas.edu>
2021-08-30 19:21:22 -0500

    MovingAverage codes are added

---
+++ Data_structures/MovingAvg_CircQueue.py
@@ -0,0 +1,16 @@
+class MovingAverage:
+    def __init__(self, size: int):
+        self.size = size
+        self.queue = [0]*self.size
+        self.window_sum = 0
+        self.head = 0
+        self.count = 0
+        
+
+    def next(self, val: int) -> float:
+        self.count+=1
+        tail = (self.head +1) % self.size
+        self.window_sum = self.window_sum + val - self.queue[tail]
+        self.head = (self.head +1) % self.size
+        self.queue [ self.head] = val
+        return self.window_sum / min (self.size, self.count)
