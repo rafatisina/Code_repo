38aa7ac18d5e79127b3fc16a02a83947b233ab74
Sina <rafati@utexas.edu>
2021-09-02 00:19:09 -0500

    array_codes_1 added

---
+++ Arrays/threepointer_merge.py
@@ -0,0 +1,15 @@
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        nums1copy = nums1[:m]
+        p1=0
+        p2=0
+
+
+        for p in range(n+m):
+
+            if p2>=n or (p1<m and nums1copy[p1] <= nums2[p2]):
+                nums1[p] = nums1copy[p1]
+                p1+=1
+            else:
+                nums1[p] = nums2[p2]
+                p2+=1
