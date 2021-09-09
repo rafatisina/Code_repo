e4446bd5a1215c73f7830390272243e5d0de3754
Sina <rafati@utexas.edu>
2021-09-08 23:55:23 -0500

    cansum added

---
+++ DynamicProgramming/cansum.py
@@ -0,0 +1,19 @@
+def cansum(target: int, lst, memo={}):
+    if target in memo:
+        return memo[target]
+
+    if target == 0:
+        return True
+    if target < 0:
+        return False
+    for v in lst:
+        remainder = target - v
+        if cansum(remainder, lst, memo):
+            memo[target] = True
+            return True
+    memo[target] = False
+    return False
+
+
+result = cansum(3500, [4, 2])
+print(result)
